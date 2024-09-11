598914b59d85293938779d7e295f29e32568d3c0
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-11 16:23:51 -0400

    Update clickcease.tsx

--- app/clickcease.tsx
+++ app/clickcease.tsx
@@ -5,11 +5,7 @@ import Script from "next/script";
 const ClickCease = () => {
     return (
         <>
-            <Script id = "clickcease"> {`
-                var script = document.createElement('script');
-                script.async = true; script.type = 'text/javascript';
-                var target = 'https://www.clickcease.com/monitor/stat.js';
-                script.src = target;var elem = document.head;elem.appendChild(script);
+
             `}
             </Script>
             <noscript>
